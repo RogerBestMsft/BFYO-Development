cbcad3319eac7ccb8aec0462a700d163602d6d03
RBestMSFT <rbest@microsoft.com>
2024-10-24 15:01:27 -0700

    add debugging

--- SampleApplication/infrastructure/devbox_tasks/InstallSqlManStudio/InstallSqlManStudio.ps1
+++ SampleApplication/infrastructure/devbox_tasks/InstallSqlManStudio/InstallSqlManStudio.ps1
@@ -1,4 +1,14 @@
 
+# Write out debug information
+$outputFilePath = "InstallSqlManStudio.txt"
+
+# Define the content to be written to the file
+$content = "File created $(Get-Date)."
+
+# Write the content to the file
+Set-Content -Path $outputFilePath -Value $content
+
+
 # Define the URL of the file you want to download
 $url = "https://aka.ms/ssmsfullsetup"
 
@@ -11,3 +21,16 @@ Invoke-WebRequest -Uri $url -OutFile $destination
 
 #https://aka.ms/ssmsfullsetup
 .\SSMS-Setup-ENU.exe /quiet
+
+
+$outputFilePath2 = "InstallSqlManStudio2.txt"
+
+# Define the content to be written to the file
+$content2 = "File created $(Get-Date)."
+
+# Write the content to the file
+Set-Content -Path $outputFilePath2 -Value $content2
+
+$outputFilePath3 = "C:\Temp\Installsqlmanstudio3.txt"
+$content3 = "Execution $(Get-Location)"
+Set-Content -Path $outputFilePath3 -Value $content3
