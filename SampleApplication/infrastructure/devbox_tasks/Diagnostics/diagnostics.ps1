ebf59cd71dbad6083eae5912a7becc11320891b1
RBestMSFT <rbest@microsoft.com>
2024-12-06 09:46:25 -0800

    Add diagnostics

--- SampleApplication/infrastructure/devbox_tasks/Diagnostics/diagnostics.ps1
+++ SampleApplication/infrastructure/devbox_tasks/Diagnostics/diagnostics.ps1
@@ -1,2 +1,4 @@
-New-Item c:\temp -ItemType Directory
-New-Item c:\temp\diagnostics.txt -ItemType File
+New-Item "c:\temp" -ItemType Directory
+New-Item "c:\temp\diagnostics.txt" -ItemType File
+
+Add-Content -Path "c:\temp\diagnostics.txt" -Value ${Get-Date}
